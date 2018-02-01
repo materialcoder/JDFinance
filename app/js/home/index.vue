839d30e31dba237bbb856c235dac5e291c8a8762
materialcoder <1028106985@qq.com>
2018-02-01 20:32:58 +0800

    home

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,18 +3,36 @@
         <heador/>
         <slider/>
         <novice/>
+        <borrow/>
+        <money/>
+        <product/>
+        <life/>
+        <footor/>
+        <navbar/>
     </div>
 </template>
 
 <script>
 import Heador from '../public/header.vue'
+import Footor from '../public/footer'
+import Navbar from '../public/navbar'
 import Slider from './hslider'
 import Novice from './novice'
+import Borrow from './borrow'
+import Money from './money'
+import Product from './product'
+import Life from './life'
 export default {
   components: {
     Heador,
+    Footor,
+    Navbar,
     Slider,
-    Novice
+    Novice,
+    Borrow,
+    Money,
+    Product,
+    Life
   }
 }
 </script>
