2bcd0d64819c26aa3f6a695c0727871e6fe83a33
materialcoder <1028106985@qq.com>
2018-02-02 11:40:24 +0800

    ious

---
+++ app/js/ious/index.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <heador/>
+        <footor/>
+        <navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header";
+import Footor from "../public/footer";
+import Navbar from "../public/navbar";
+export default {
+  components: {
+    Heador,
+    Footor,
+    Navbar
+  }
+};
+</script>
+
+<style lang="scss" module>
+
+</style>
